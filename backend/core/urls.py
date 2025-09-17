2071ae7916af6502482ed630514c8e248f6a28f4
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2025-09-18 03:41:56 +0600

    user_type and fetch whole info

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from dj_rest_auth.registration.views import RegisterView
+
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -36,6 +38,7 @@ urlpatterns = [
      path("admin/", admin.site.urls),
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("auth/", include('django.contrib.auth.urls')),
     path(
         "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
