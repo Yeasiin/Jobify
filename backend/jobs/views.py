c37e5f5bd878a76a70e67d7153462212dd43d883
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2025-09-22 02:39:28 +0600

    delete response

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Job
 from .serializers import JobSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class  = JobSerializer
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance_id = instance.id  # store id before deleting
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Job deleted successfully", "id": instance_id},
+            status=status.HTTP_200_OK
+        )
