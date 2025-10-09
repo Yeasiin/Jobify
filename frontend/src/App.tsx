1b86e5b5cdfc4c18ad41f41a9eb92b18c61e8567
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2025-10-09 12:22:49 +0600

    path fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router";
-import Login from "./pages/Login";
-import Registration from "./pages/Registration";
-import ForgotPassword from "./pages/ForgotPassword";
-import Home from "./pages/Home";
-import NotFound404 from "./pages/404";
-import ProtectedRoute from "./components/ProtectedRoute";
-import AnonymousRoute from "./components/AnonymousRoute";
-import CreateJob from "./pages/CreateJob";
-import EmployerDashboard from "./pages/EmployerDashboard";
-import JobSeekerDashboard from "./pages/JobSeekerDashboard";
-import PreviewJob from "./pages/PreviewJob";
-import Jobs from "./pages/Jobs";
-import JobApplications from "./pages/JobApplications";
+import Login from "@/pages/Login";
+import Registration from "@/pages/Registration";
+import ForgotPassword from "@/pages/ForgotPassword";
+import Home from "@/pages/Home";
+import NotFound404 from "@/pages/404";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import AnonymousRoute from "@/components/AnonymousRoute";
+import CreateJob from "@/pages/CreateJob";
+import EmployerDashboard from "@/pages/EmployerDashboard";
+import JobSeekerDashboard from "@/pages/JobSeekerDashboard";
+import PreviewJob from "@/pages/PreviewJob";
+import Jobs from "@/pages/Jobs";
+import JobApplications from "@/pages/JobApplications";
 
 function App() {
   return (
